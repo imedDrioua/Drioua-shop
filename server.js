d937acfdb2609f24ca1e482d56fb38b59de14f30
imedDrioua <ii_drioua@esi.dz>
2020-06-24 16:41:10 -0700

    finishing the app

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const app=express();
  app.use(cors());
  if(process.env.NODE_ENV === "production")
  {
-     app.use(express.static(path.join(__dirname , "client/public")));
+     app.use(express.static(path.join(__dirname , "client/build")));
 
      app.get("*",(req , res )=>{
-          res.sendFile(path.join(__dirname,"client/buid/","index.html"));
+          res.sendFile(path.join(__dirname,"client/build/","index.html"));
      });
  }
  app.listen(port , error=>{
